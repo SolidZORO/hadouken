43f9c394886fd106a91aae2257066dc4e0d47bbd
SolidZORO <SolidZORO@icloud.com>
2014-12-07 03:29:00 +0800

    修復個別錯誤

--- run.js
+++ run.js
@@ -1,75 +1,26 @@
+// 引用
 var fs = require("fs");
 var parseString = require('xml2js').parseString;
 var jade = require('jade');
 var gm = require('gm');
-var markdown = require("markdown").markdown;
 var md = require("node-markdown").Markdown;
-// var sanitize = require('sanitize-caja');
-// var sanitizeHtml = require('sanitize-html');
-var sanitizeHtml = require('sanitize-html');
-var validator = require('validator');
 
-// 日誌目錄
+// 目錄
 // var DIR_ENTRIES = './attachment/entries/';
 var DIR_ENTRIES = '/Users/SolidZORO/Library/Mobile\ Documents/5U8NS4GX82\~com\~dayoneapp\~dayone/Documents/Journal_dayone/entries/';
-
-// 附件目錄
 var DIR_PHOTOS_O = './attachment/origin/';
 var DIR_PHOTOS_T = './attachment/thumbnail/';
 
 // 配置的主題
 var THEME_NAME = 'sample';
 
+
+
+
+
 // 所有日誌
 var entries = [];
-var entrie_tags_book = [];
-
-
-
-
-
-
-// 判斷字符是否已在數組中 2
-function in_array(needle, haystack, argStrict) {
-    //  discuss at: http://phpjs.org/functions/in_array/
-    // original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // improved by: vlado houba
-    // improved by: Jonas Sciangula Street (Joni2Back)
-    //    input by: Billy
-    // bugfixed by: Brett Zamir (http://brett-zamir.me)
-    //   example 1: in_array('van', ['Kevin', 'van', 'Zonneveld']);
-    //   returns 1: true
-    //   example 2: in_array('vlado', {0: 'Kevin', vlado: 'van', 1: 'Zonneveld'});
-    //   returns 2: false
-    //   example 3: in_array(1, ['1', '2', '3']);
-    //   example 3: in_array(1, ['1', '2', '3'], false);
-    //   returns 3: true
-    //   returns 3: true
-    //   example 4: in_array(1, ['1', '2', '3'], true);
-    //   returns 4: false
-
-    var key = '',
-        strict = !!argStrict;
-
-    //we prevent the double check (strict && arr[key] === ndl) || (!strict && arr[key] == ndl)
-    //in just one for, in order to improve the performance
-    //deciding wich type of comparation will do before walk array
-    if (strict) {
-        for (key in haystack) {
-            if (haystack[key] === needle) {
-                return true;
-            }
-        }
-    } else {
-        for (key in haystack) {
-            if (haystack[key] == needle) {
-                return true;
-            }
-        }
-    }
-
-    return false;
-}
+var entrie_tags = [];
 
 
 
@@ -82,7 +33,6 @@ function in_array(needle, haystack, argStrict) {
 // 遍歷文件 S
 fs.readdirSync(DIR_ENTRIES).forEach(function (file, i) {
 
-    // 單篇日誌
     var entrie = {};
 
     var entrie_xml = fs.readFileSync(DIR_ENTRIES + file, 'utf-8');
@@ -96,6 +46,7 @@ fs.readdirSync(DIR_ENTRIES).forEach(function (file, i) {
 
         // 日期賦值
         entrie.date = entrie_json.plist.dict[0].date[0];
+
         // 處理時間格式，原格式為 2014-11-23T15:32:15Z
         entrie.date = entrie.date.replace(/T/, " ");
         entrie.date = entrie.date.replace(/Z/, '');
@@ -112,18 +63,19 @@ fs.readdirSync(DIR_ENTRIES).forEach(function (file, i) {
         var s = entrie.url.getSeconds() < 10 ? '0' + entrie.url.getSeconds() : entrie.url.getSeconds();
         entrie.url = Y + '-' + M + '-' + D + '-' + h + m + s;
 
-        // console.log(entrie.date_timestamp);
 
-        // 文章內容
-        var e_string = entrie_json.plist.dict[0].string[0];
+
+
 
         // 把文章內容一分為二（標題與內容）S
+        var e_string = entrie_json.plist.dict[0].string[0];
         if (e_string !== '') {
 
             // 匹配只有 標題
             var reg1 = /(.{2,})(?:\n\n)?/;
             // 匹配有 標題 和 內容
             var reg2 = /(.{2,})(?:\n\n)?((?:.|\n)*)?/;
+
             // 結果只有 標題
             var result1 = reg1.exec(e_string);
             // 結果有 標題和內容
@@ -132,10 +84,8 @@ fs.readdirSync(DIR_ENTRIES).forEach(function (file, i) {
 
             // 如果找到標題，先賦值
             if (result1[0]) {
-
                 // 標題賦值
                 entrie.title = result1[0].replace(/[\r\n]/g, "");
-
 
                 // 與日誌匹配的圖片路徑拼接
                 var e_photo = DIR_PHOTOS_O + entrie.id + '.jpg';
@@ -143,63 +93,66 @@ fs.readdirSync(DIR_ENTRIES).forEach(function (file, i) {
                 if (fs.existsSync(e_photo)) {
 
                     // 原圖
-                    entrie.photo = e_photo + '.jpg';
+                    entrie.photo = e_photo;
 
                     // 480
                     gm(e_photo).resize(480).write(DIR_PHOTOS_T + '480/' + entrie.id + '.jpg', function (err) {
                         if (err) return console.dir(arguments);
                     });
                     entrie.photo_480 = DIR_PHOTOS_T + '480/' + entrie.id + '.jpg';
-
-
-                    // // 1024
-                    // gm(e_photo).resize(1280).write(DIR_PHOTOS_T + '1280/' + entrie.id + '.jpg', function (err) {
-                    //     if (err) return console.dir(arguments);
-                    // });
-                    // entrie.photo_1280 = DIR_PHOTOS_T + '1280/' + entrie.id + '.jpg';
-
                 };
 
 
-
-
-                // 內容賦制
+                // 日誌內容賦值，MD轉HTML
                 if (result2[2]) {
                     var e_text = result2[2];
                 } else {
                     var e_text = "這篇日誌沒有內容。";
                 };
                 entrie.text = md(e_text);
-                // entrie.text = sanitizeHtml(entrie.text);
-                // entrie.text = e_text;
             }
         }
-        // 把文章內容一分為二（標題與內容）S
+        // 把文章內容一分為二（標題與內容）E
 
 
 
-        // 遍歷tag標籤
+        // 遍歷tag標籤 S
         if (typeof (entrie_json.plist.dict[0].array) !== "undefined") {
-
-            // 這裏是for給全局tags_book的
             entrie.tags_arr = entrie_json.plist.dict[0].array[0].string;
             for (var i in entrie.tags_arr) {
-                // 如果entrie_tags_book數組中沒有當前tag，就push到數組裏。
-                // if (!inArray(entrie.tags_arr[i], entrie_tags_book)) {
-                if (!in_array(entrie.tags_arr[i], entrie_tags_book)) {
-                    // console.log(entrie.tags_arr[i]);
-                    entrie_tags_book.push(entrie.tags_arr[i]);
+
+                // 判斷字符是否已在數組中
+                function in_array(needle, haystack, argStrict) {
+                    var key = '',
+                        strict = !!argStrict;
+
+                    if (strict) {
+                        for (key in haystack) {
+                            if (haystack[key] === needle) {
+                                return true;
+                            }
+                        }
+                    } else {
+                        for (key in haystack) {
+                            if (haystack[key] == needle) {
+                                return true;
+                            }
+                        }
+                    }
+
+                    return false;
+                }
+
+                // 如果entrie_tags數組中沒有當前tag，就push到數組裏。
+                if (!in_array(entrie.tags_arr[i], entrie_tags)) {
+                    entrie_tags.push(entrie.tags_arr[i]);
                 }
             }
-
             // 這裏是for給當前文章tags的
             entrie.tags = entrie_json.plist.dict[0].array[0].string;
-            // console.log(typeof (entrie.tags));
         }
+        // 遍歷tag標籤 E
 
-        // console.log(entrie_json.plist.dict[0].array[0].string);
-
-        // 把單個日誌帶入entries數組
 
     });
     // 處理日誌 E
@@ -215,6 +168,9 @@ fs.readdirSync(DIR_ENTRIES).forEach(function (file, i) {
     fs.writeFileSync('./day/' + entrie.url + '.html', html_entrie);
     // 渲染並寫入 E
 
+
+
+    // 單篇日誌裝入日誌合集
     entries[i] = entrie;
 
 });
