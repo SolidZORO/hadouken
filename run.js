dadac528d00dbc7c66aba77bb44b26d5cb8b6f78
SolidZORO <SolidZORO@icloud.com>
2014-12-12 01:04:51 +0800

    加入隱私日誌功能，只需在tag寫入「秘密」即不會生成html

--- run.js
+++ run.js
@@ -1,4 +1,10 @@
 /*
+     __  _____    ____  ____  __  ____ __ _______   __
+    / / / /   |  / __ \/ __ \/ / / / //_// ____/ | / /
+   / /_/ / /| | / / / / / / / / / / ,<  / __/ /  |/ /
+  / __  / ___ |/ /_/ / /_/ / /_/ / /| |/ /___/ /|  /
+ /_/ /_/_/  |_/_____/\____/\____/_/ |_/_____/_/ |_/
+
  HADOUKEN是一個用nodejs寫的dayone日誌轉換工具，用於把dayone的日誌處理成html。
  SolidZORO 2014-12-10 19:53:13
 */
@@ -25,16 +31,23 @@ var CONFIG = {
     THEME: 'ryu',
     // OSX系統用戶名
     USERNAME: 'SolidZORO',
-    // 縮略圖大小（自定義）
-    BANNER_THUMB_SIZE: '480',
-    // 几篇日志翻一页？
-    PAGE_NUM: 15,
     // 站點名稱
     SITE_NAME: 'SolidZORO',
     // 站點說明
     SITE_DESC: '黑魔法師。',
     // 站點網址
     SITE_URL: 'http://solidzoro.com',
+    // 輸出首頁的名字，如果你的站點index.html已被佔用，可改成其他。
+    INDEX: 'index.html',
+    // 縮略圖大小（自定義）
+    BANNER_THUMB_SIZE: '480',
+    // 几篇日志翻一页？
+    PAGE_NUM: 15,
+    // 如果日誌中有這個Tag，則不生成到html中。
+    KEEP_LOCAL: '秘密',
+    // 自定義閱讀
+    READ_MORE: '閱讀全文',
+
 
     ATTACHMENT: 'attachment',
     BANNER_ORIGIN: 'origin',
@@ -134,6 +147,13 @@ function in_array(needle, haystack, argStrict) {
 // console.log(PATH.DAYONE_ENTRIES);
 // console.log(PATH.DAYONE_PHOTOS);
 
+
+
+
+
+
+
+
 /////////////////////////////////////////////////////////////
 // 如果dayone日誌目錄存在，才開始運行。
 /////////////////////////////////////////////////////////////
@@ -215,11 +235,6 @@ if (fs.existsSync(PATH.DAYONE_ENTRIES)) {
         // 假如找到了dayone日誌文件，就開始處理它們 S
         if (result_dayone) {
 
-
-            // [對象] 每一個日誌都是一個對象，這個{}一定要放在這裏，放在外面就bug了。大坑啊！
-            // TODO，如果知道原理的同學可以告訴我一下原理： solidzoro __ icloud __ com
-            var day = {};
-
             // 讀取dayone的日誌文件（其實就是xml）
             var day_xml = fs.readFileSync(PATH.DAYONE_ENTRIES + file, 'utf-8');
 
@@ -229,143 +244,157 @@ if (fs.existsSync(PATH.DAYONE_ENTRIES)) {
                     return 'err';
                 };
 
-                // 狀態，如果日誌沒有狀態，就設定為空''
-                if (!in_array(day_json.plist.dict[0].string[0], ACTIVITY)) {
-                    day_json.plist.dict[0].string.splice(0, 0, '');
-                }
-                day.activity = day_json.plist.dict[0].string[0];
 
 
-                // ID
-                day.id = day_json.plist.dict[0].string[2];
+                // 如果日誌tags裏有「秘密」或你自定義的隱私keyword，就排除掉這篇日誌 S
+                if (!in_array(CONFIG.KEEP_LOCAL, day_json.plist.dict[0].array[0].string)) {
 
+                    // [對象] 每一個日誌都是一個對象，這個{}一定要放在這裏，放在外面就bug了。大坑啊！
+                    // TODO，如果知道原理的同學可以告訴我一下原理： solidzoro __ icloud __ com
+                    var day = {};
 
-                // 日期（原格式為 2014-11-23T15:32:15Z）
-                day.source_date = day_json.plist.dict[0].date[0];
-
-
-                // 處理時間
-                day.source_date = day.source_date.replace(/T/, " ");
-                day.source_date = day.source_date.replace(/Z/, '');
-
-                day.timestamp = new Date(day.source_date);
-                day.timestamp = day.timestamp.getTime() / 1000;
-                day.source_timestamp = new Date(day.timestamp * 1000);
-
-                var day_date_Y = day.source_timestamp.getFullYear();
-                var day_date_M = (day.source_timestamp.getMonth() + 1) < 10 ? '0' + (day.source_timestamp.getMonth() + 1) : day.source_timestamp.getMonth() + 1;
-                var day_date_D = day.source_timestamp.getDate() < 10 ? '0' + day.source_timestamp.getDate() : day.source_timestamp.getDate();
-                var day_date_h = day.source_timestamp.getHours() < 10 ? '0' + day.source_timestamp.getHours() : day.source_timestamp.getHours();
-                var day_date_m = day.source_timestamp.getMinutes() < 10 ? '0' + day.source_timestamp.getMinutes() : day.source_timestamp.getMinutes();
-                var day_date_s = day.source_timestamp.getSeconds() < 10 ? '0' + day.source_timestamp.getSeconds() : day.source_timestamp.getSeconds();
-
-                day.date = day_date_Y + '-' + day_date_M + '-' + day_date_D;
-                day.date_zh = day_date_Y + '年 ' + day_date_M + '月 ' + day_date_D + '日';
-
-                // URL
-                day.url = day_date_Y + '-' + day_date_M + '-' + day_date_D + '-' + day_date_h + day_date_m + day_date_s;
-
-
-
-                // 把只有一個字段的日誌分解成 標題與內容，順便把配圖也找一下 S
-                day.source_title_and_content = day_json.plist.dict[0].string[1];
-
-                if (day.source_title_and_content !== '') {
-
-                    // 正則 標題
-                    var regex_title = /(.{2,})(?:\n\n)?/;
-                    // 正則 標題+內容
-                    var regex_title_and_content = /(.{2,})(?:\n\n)?((?:.|\n)*)?/;
-
-                    // 結果 標題
-                    var result_title = regex_title.exec(day.source_title_and_content);
-                    // 結果 標題+內容
-                    var result_title_and_content = regex_title_and_content.exec(day.source_title_and_content);
-
-
-                    // 假如日誌有標題的話
-                    if (result_title[0]) {
-
-                        // 標題賦值（去掉還行）
-                        day.title = result_title[0].replace(/[\r\n]/g, "");
-
-                        // 處理日誌內容，以及markdown化 S
-                        if (result_title_and_content[2]) {
-                            // <!--\smore\s-->
-                            day.source_content = result_title_and_content[2];
-
-                            // 擷取more
-                            var regex_content_more = /((:?.|\n)*?)<!-- more -->/;
-                            var result_content_more = regex_content_more.exec(day.source_content);
-
-                            if (result_content_more !== null) {
-                                var more = '[閱讀全文](../day/' + day.url + '.html)';
-                                day.content_list_html = md(result_content_more[1] + more);
-                            }
-
-                            day.content = day.source_content;
-                        } else {
-                            day.content = "這篇日誌沒有內容。";
-                        };
-
-                        day.content_html = md(day.content);
-                        // 處理日誌內容，以及markdown化 S
-
-
-
-                        // 拼接日誌配圖路徑（都是jpg的，如果不是jpg，dayone也會轉成jpg）
-                        day.source_banner = PATH.BANNER_ORIGIN + day.id + '.jpg';
-
-                        // 如果找到日誌配圖圖片，就賦值並壓縮小圖 S
-                        if (fs.existsSync(day.source_banner)) {
-
-                            // 原圖
-                            day.banner_origin = day.source_banner;
-
-                            // 壓縮成指定大小的圖片
-                            gm(day.banner_origin).resize(CONFIG.BANNER_THUMB_SIZE).write(PATH.BANNER_THUMB_SIZE + day.id + '.jpg', function (err) {
-                                if (err) {
-                                    return console.dir(arguments);
-                                }
-                            });
-
-                            // 縮略圖
-                            day.banner_thumb = PATH.BANNER_THUMB_SIZE + day.id + '.jpg';
-
-                        };
-                        // 如果找到日誌配圖圖片，就賦值並壓縮小圖 E
-
-
+                    // 狀態，如果日誌沒有狀態，就設定為空''
+                    if (!in_array(day_json.plist.dict[0].string[0], ACTIVITY)) {
+                        day_json.plist.dict[0].string.splice(0, 0, '');
                     }
-                }
-                // 把文章內容一分為二（標題與內容）E
+                    day.activity = day_json.plist.dict[0].string[0];
+
+
+                    // ID
+                    day.id = day_json.plist.dict[0].string[3];
+
+
+                    // 日期（原格式為 2014-11-23T15:32:15Z）
+                    day.source_date = day_json.plist.dict[0].date[0];
+
+
+                    // 處理時間
+                    day.source_date = day.source_date.replace(/T/, " ");
+                    day.source_date = day.source_date.replace(/Z/, '');
+
+                    day.timestamp = new Date(day.source_date);
+                    day.timestamp = day.timestamp.getTime() / 1000;
+                    day.source_timestamp = new Date(day.timestamp * 1000);
+
+                    var day_date_Y = day.source_timestamp.getFullYear();
+                    var day_date_M = (day.source_timestamp.getMonth() + 1) < 10 ? '0' + (day.source_timestamp.getMonth() + 1) : day.source_timestamp.getMonth() + 1;
+                    var day_date_D = day.source_timestamp.getDate() < 10 ? '0' + day.source_timestamp.getDate() : day.source_timestamp.getDate();
+                    var day_date_h = day.source_timestamp.getHours() < 10 ? '0' + day.source_timestamp.getHours() : day.source_timestamp.getHours();
+                    var day_date_m = day.source_timestamp.getMinutes() < 10 ? '0' + day.source_timestamp.getMinutes() : day.source_timestamp.getMinutes();
+                    var day_date_s = day.source_timestamp.getSeconds() < 10 ? '0' + day.source_timestamp.getSeconds() : day.source_timestamp.getSeconds();
+
+                    day.date = day_date_Y + '-' + day_date_M + '-' + day_date_D;
+                    day.date_zh = day_date_Y + '年 ' + day_date_M + '月 ' + day_date_D + '日';
+
+                    // URL
+                    day.url = day_date_Y + '-' + day_date_M + '-' + day_date_D + '-' + day_date_h + day_date_m + day_date_s;
 
 
 
-                // 遍歷日誌Tag S
-                if (typeof (day_json.plist.dict[0].array) !== "undefined") {
+                    // 把只有一個字段的日誌分解成 標題與內容，順便把配圖也找一下 S
+                    day.source_title_and_content = day_json.plist.dict[0].string[1];
 
-                    day.source_tags = day_json.plist.dict[0].array[0].string;
+                    if (day.source_title_and_content !== '') {
 
-                    // 這裏for只是用來遍歷tag，把all_tag裏不存在的tag壓到all_tag裏
-                    for (var i in day.source_tags) {
-                        if (!in_array(day.source_tags[i], all_tag)) {
-                            all_tag.push(day.source_tags[i]);
+                        // 正則 標題
+                        var regex_title = /(.{2,})(?:\n\n)?/;
+                        // 正則 標題+內容
+                        var regex_title_and_content = /(.{2,})(?:\n\n)?((?:.|\n)*)?/;
+
+                        // 結果 標題
+                        var result_title = regex_title.exec(day.source_title_and_content);
+                        // 結果 標題+內容
+                        var result_title_and_content = regex_title_and_content.exec(day.source_title_and_content);
+
+
+                        // 假如日誌有標題的話
+                        if (result_title[0]) {
+
+                            // 標題賦值（去掉還行）
+                            day.title = result_title[0].replace(/[\r\n]/g, "");
+
+                            // 處理日誌內容，以及markdown化 S
+                            if (result_title_and_content[2]) {
+                                day.source_content = result_title_and_content[2];
+
+                                // 擷取more
+                                var regex_content_more = /((:?.|\n)*?)<!--\s?more\s?-->/;
+                                var result_content_more = regex_content_more.exec(day.source_content);
+
+                                if (result_content_more !== null) {
+                                    // var more = '[' + CONFIG.READ_MORE + '](../day/' + day.url + '.html){class="read_more"}';
+                                    var more = '<a href="../day/' + day.url + '.html" class="read_more">' + CONFIG.READ_MORE + '</a>';
+                                    day.content_list_html = md(result_content_more[1] + more);
+                                }
+
+                                day.content = day.source_content;
+                            } else {
+                                day.content = "這篇日誌沒有內容。";
+                            };
+
+                            day.content_html = md(day.content);
+                            // 處理日誌內容，以及markdown化 S
+
+
+
+                            // 拼接日誌配圖路徑（都是jpg的，如果不是jpg，dayone也會轉成jpg）
+                            day.source_banner = PATH.BANNER_ORIGIN + day.id + '.jpg';
+
+
+                            // 如果找到日誌配圖圖片，就賦值並壓縮小圖 S
+                            if (fs.existsSync(day.source_banner)) {
+
+                                // 原圖
+                                day.banner_origin = day.source_banner;
+
+                                // 壓縮成指定大小的圖片
+                                gm(day.banner_origin).resize(CONFIG.BANNER_THUMB_SIZE).write(PATH.BANNER_THUMB_SIZE + day.id + '.jpg', function (err) {
+                                    if (err) {
+                                        return console.dir(arguments);
+                                    }
+                                });
+
+                                // 縮略圖
+                                day.banner_thumb = PATH.BANNER_THUMB_SIZE + day.id + '.jpg';
+
+                            };
+                            // 如果找到日誌配圖圖片，就賦值並壓縮小圖 E
+
+
                         }
                     }
+                    // 把文章內容一分為二（標題與內容）E
 
-                    // 這裏為什麼有這個？？
-                    day.tags = day.source_tags;
+
+
+                    // 遍歷日誌Tag S
+                    if (typeof (day_json.plist.dict[0].array) !== "undefined") {
+
+                        day.source_tags = day_json.plist.dict[0].array[0].string;
+
+                        // 這裏for只是用來遍歷tag，把all_tag裏不存在的tag壓到all_tag裏
+                        for (var i in day.source_tags) {
+                            if (!in_array(day.source_tags[i], all_tag)) {
+                                all_tag.push(day.source_tags[i]);
+                            }
+                        }
+
+                        // 這裏為什麼有這個？？
+                        day.tags = day.source_tags;
+                    }
+                    // 遍歷日誌Tag E
+
+
+                    // 全部處理完後就賦值給日誌數組
+                    all_day[all_day_length] = day;
+                    all_day_length++;
                 }
-                // 遍歷日誌Tag E
+                // 如果日誌tags裏有「秘密」或你自定義的隱私keyword，就排除掉這篇日誌 E
 
             });
             // 處理日誌，簡直就是浩大的工程！ E
 
 
-            // 全部處理完後就賦值給日誌數組
-            all_day[all_day_length] = day;
-            all_day_length++;
 
             // console.log(day.title);
             // all_day.push(day);
@@ -428,13 +457,14 @@ if (fs.existsSync(PATH.DAYONE_ENTRIES)) {
     var jade_index = jade.compileFile('./themes/' + CONFIG.THEME + '/index.jade');
     var html_data = jade_index({
         HOME_PATH: './',
+        BODY_CLASS: 'katana-list katana-index',
         CONFIG: CONFIG,
         PATH: PATH,
         totle_page_num: totle_page_num,
         current_page_num: 1,
         datas: all_day_index
     });
-    fs.writeFileSync('index.html', html_data);
+    fs.writeFileSync(CONFIG.INDEX, html_data);
     // 首頁渲染並寫入 E
 
 
@@ -453,11 +483,13 @@ if (fs.existsSync(PATH.DAYONE_ENTRIES)) {
         // jade模板賦值
         var html_data = jade_day({
             HOME_PATH: '../',
+            BODY_CLASS: 'katana-detaile katana-day',
             CONFIG: CONFIG,
             PATH: PATH,
             title: all_day[i].title,
             data: all_day[i]
         });
+
 
         if (!fs.existsSync(PATH.DAY)) {
             fs.mkdirSync(PATH.DAY);
@@ -485,6 +517,7 @@ if (fs.existsSync(PATH.DAYONE_ENTRIES)) {
         one_page.push(all_day.slice(i, i + CONFIG.PAGE_NUM));
         var html_data = jade_page({
             HOME_PATH: '../',
+            BODY_CLASS: 'katana-list katana-page',
             CONFIG: CONFIG,
             PATH: PATH,
             totle_page_num: totle_page_num,
@@ -533,6 +566,7 @@ if (fs.existsSync(PATH.DAYONE_ENTRIES)) {
         // jade模板賦值
         var html_data = jade_tag({
             HOME_PATH: '../',
+            BODY_CLASS: 'katana-list katana-tag',
             CONFIG: CONFIG,
             PATH: PATH,
             title: all_tag[t],
